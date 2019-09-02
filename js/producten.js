f50564c4038230d96252094fbb9acf93b0d253ef
JRJman <41746794+JRJman@users.noreply.github.com>
2019-09-02 14:53:24 +0200

    producten paginas toegevoegd

---
+++ js/producten.js
@@ -0,0 +1,7 @@
+let button = document.getElementById('kopen');
+
+button.addEventListener('click', kopen);
+
+function kopen(){
+    window.location.assign("file:///C:/Users/user/Documents/ma/bewijzenmap/periode2.1/superstore80/cart.html");
+}

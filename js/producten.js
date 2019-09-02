867c4caf83d574bb6168d708bd22c8190fc2fdac
JRJman <41746794+JRJman@users.noreply.github.com>
2019-09-02 14:58:19 +0200

    links verbeterd

--- js/producten.js
+++ js/producten.js
@@ -3,5 +3,5 @@ let button = document.getElementById('kopen');
 button.addEventListener('click', kopen);
 
 function kopen(){
-    window.location.assign("file:///C:/Users/user/Documents/ma/bewijzenmap/periode2.1/superstore80/cart.html");
+    window.location.assign("../cart.html");
 }

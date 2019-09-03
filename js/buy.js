3c81a9253188ce3e8799d4baa9440b8207c91aab
florianmac <nairolf3>
2019-09-03 11:39:38 +0200

    ry

--- js/buy.js
+++ js/buy.js
@@ -1,7 +1,7 @@
 
-var id = ["huisnummer","postcode"];
+var id = ["huisnummer","postcode","banknummer","really"];
 let hello = 1;
-
+var counter = 0;
 localStorage.clear();
 document.getElementById('a').addEventListener("keypress", test);
 function test(){
@@ -10,8 +10,18 @@ function test(){
 console.log(key);
   switch(key){
     case 13:
+    console.log(id[counter]);
 
+document.getElementById(id[counter]).style.property = visibility:visible;
+
+
+counter+=1;
+if(counter>3)
+{
+  counter=0;
+}
     break;
+
 
     case 104:
     console.log("lol");

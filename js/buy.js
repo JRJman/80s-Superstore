c1f308f672447128c8f9ba7169dfa36d5ccc3146
florianmac <nairolf3>
2019-09-03 10:50:51 +0200

    wefer

--- js/buy.js
+++ js/buy.js
@@ -1,3 +1,12 @@
+// document.getElementById('response_vield').addEventListener("keypress", function);
+// function myFunction(event) {
+//   var x = event.keyCode;
+//   if (x == 27) {  // 27 is the ESC key
+//     alert ("You pressed the Escape key!");
+//   }
+// }
 
+let hello = 1;
 
 localStorage.clear();
+document.getElementById('response_vield').addEventListener("keypress", console.log(hello));

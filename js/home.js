a2c9fa7cae9d213f6730e8376e6c6ed43e83fd55
florianmac <nairolf3>
2019-09-03 15:02:38 +0200

    erer

--- js/home.js
+++ js/home.js
@@ -9,3 +9,25 @@ function ToHome(){
 function ToCart(){
     window.location.assign("cart.html");
 }
+
+
+
+var counter = 1;
+document.getElementById('a').addEventListener("keypress", test);
+
+function test(){
+  var key = event.keyCode;
+
+console.log(key);
+  switch(key){
+
+
+    case 104:
+    console.log("lol");
+    window.location.href ='home.html';
+    break;
+    case 110:
+    window.scroll(0,1000);
+    break;
+  }
+}
